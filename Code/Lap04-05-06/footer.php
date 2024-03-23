a5a306fe19852a2d1667fb4b5a24be1c0f044815
Nguyễn Khánh An <122146985+khanhan1234453@users.noreply.github.com>
2024-03-23 11:10:25 +0700

    Add files via upload

---
+++ Code/Lap04-05-06/footer.php
@@ -0,0 +1,10 @@
+</div>
+</div>
+<div id="wrapper" class="container">
+<footer>
+Comeback <a href="/lab04-05-06/index.php">Home page</a><br />
+&#169; 2022 - Ankhseram1412@gmail.com
+</footer>
+</div>
+</body>
+</html>
